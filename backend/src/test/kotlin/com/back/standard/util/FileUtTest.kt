3bd05999b6d5e5d25adc3e69780d3aad41b373db
jhs512 <jangka512@gmail.com>
2025-09-15 10:10:22 +0900

    work

---
+++ backend/src/test/kotlin/com/back/standard/util/FileUtTest.kt
@@ -0,0 +1,18 @@
+package com.back.standard.util
+
+import com.back.standard.sampleResource.SampleResource
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FileUtTest {
+    @Test
+    @DisplayName("SampleResource.IMG_JPG_SAMPLE1.fileName")
+    fun t1() {
+        assertThat(SampleResource.IMG_JPG_SAMPLE1.fileName).isEqualTo("sample1-200x300.jpg")
+    }
+}
